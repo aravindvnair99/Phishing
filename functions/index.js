47bc3a4dd5a0a2c892c7698850d5f83ef4de53f0
Aravind V Nair <22199259+aravindvnair99@users.noreply.github.com>
2019-04-16 00:32:53 +0530

    Set server

--- functions/index.js
+++ functions/index.js
@@ -1,8 +1,16 @@
-const functions = require('firebase-functions');
+const functions = require('firebase-functions'),
+    express = require('express'),
+    app = express();
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//  response.send("Hello from Firebase!");
-// });
+app.set('views', './views');
+app.set('view engine', 'ejs');
+
+app.get('/', (req, res) => {
+    res.render('index');
+});
+
+app.use((req, res, next) => {
+    res.status(404).render('404');
+});
+
+exports.app = functions.https.onRequest(app);
