c99c72328ccc74345657460665c2037b69116e0e
Aravind V Nair <22199259+aravindvnair99@users.noreply.github.com>
2019-04-16 17:24:46 +0530

    Add random CSS, JS and images

---
+++ public/js/auth.js
@@ -0,0 +1,9 @@
+var config = {
+	apiKey: "AIzaSyD9EwdILSj0JwZKd5Yq_hh8iB_8P-cyg_E",
+	authDomain: "aarohan-reg.firebaseapp.com",
+	databaseURL: "https://aarohan-reg.firebaseio.com",
+	projectId: "aarohan-reg",
+	storageBucket: "aarohan-reg.appspot.com",
+	messagingSenderId: "176714020985"
+};
+firebase.initializeApp(config);
