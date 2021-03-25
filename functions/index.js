172d1b8d7c6ca9b17368c5bb310ab5b2c6c6157b
Aravind V Nair <22199259+aravindvnair99@users.noreply.github.com>
2021-03-25 14:07:46 +0530

    Refactor
    Signed-off-by: Aravind V Nair <22199259+aravindvnair99@users.noreply.github.com>

--- functions/index.js
+++ functions/index.js
@@ -1,16 +1,16 @@
-const functions = require('firebase-functions'),
-    express = require('express'),
-    app = express();
+const functions = require("firebase-functions");
+const express = require("express");
+const app = express();
 
-app.set('views', './views');
-app.set('view engine', 'ejs');
+app.set("views", "./views");
+app.set("view engine", "ejs");
 
-app.get('/', (req, res) => {
-    res.render('index');
+app.get("/", (_req, res) => {
+	res.render("index");
 });
 
-app.use((req, res, next) => {
-    res.status(404).render('404');
+app.use((_req, res, next) => {
+	res.status(404).render("404");
 });
 
 exports.app = functions.https.onRequest(app);
