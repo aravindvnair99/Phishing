80bfa9752e1410b9dd56fb7cb78840bbd4bcd9c4
Aravind V Nair <22199259+aravindvnair99@users.noreply.github.com>
2021-03-27 10:58:35 +0530

    Cleanup and refactor
    Signed-off-by: Aravind V Nair <22199259+aravindvnair99@users.noreply.github.com>

--- public/js/auth.js
+++
@@ -1,9 +0,0 @@
-var config = {
-	apiKey: "AIzaSyDAtT_i32uZmgpGy42zRNR5bCk5duehnvI",
-	authDomain: "all-about-aravind.firebaseapp.com",
-	databaseURL: "https://all-about-aravind.firebaseio.com",
-	projectId: "all-about-aravind",
-	storageBucket: "all-about-aravind.appspot.com",
-	messagingSenderId: "249807367774"
-};
-firebase.initializeApp(config);
